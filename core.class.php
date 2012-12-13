42a58f81cb80cbd1ac8f92a0c1d92ee10d36a4fa
infinitewp <infinitewp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-13 10:38:32 +0000

    1.1.5 final commit
    git-svn-id: https://plugins.svn.wordpress.org/iwp-client/trunk@638417 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core.class.php
+++ core.class.php
@@ -259,7 +259,7 @@ class IWP_MMB_Core extends IWP_MMB_Helper
     private function get_parent_blog_option( $option_name = '' )
     {
 		global $wpdb;
-		$option = $wpdb->get_var( $wpdb->prepare( "SELECT `option_value` FROM {$wpdb->base_prefix}options WHERE option_name = '{$option_name}' LIMIT 1" ) );
+		$option = $wpdb->get_var( $wpdb->prepare( "SELECT `option_value` FROM {$wpdb->base_prefix}options WHERE option_name = %s LIMIT 1", $option_name ) );
         return $option;
     }
     
@@ -434,7 +434,7 @@ class IWP_MMB_Core extends IWP_MMB_Helper
 
         //delete plugin options, just in case
         if ($this->iwp_mmb_multisite != false) {
-			$network_blogs = $wpdb->get_results($wpdb->prepare("select `blog_id`, `site_id` from `{$wpdb->blogs}`"));
+			$network_blogs = $wpdb->get_results("select `blog_id`, `site_id` from `{$wpdb->blogs}`");
 			if(!empty($network_blogs)){
 				if( is_network_admin() ){
 					update_option('iwp_client_network_admin_install', 1);
@@ -493,7 +493,7 @@ class IWP_MMB_Core extends IWP_MMB_Helper
 		$_wp_using_ext_object_cache = false;
         
         if ($this->iwp_mmb_multisite != false) {
-			$network_blogs = $wpdb->get_col($wpdb->prepare("select `blog_id` from `{$wpdb->blogs}`"));
+			$network_blogs = $wpdb->get_col("select `blog_id` from `{$wpdb->blogs}`");
 			if(!empty($network_blogs)){
 				if( is_network_admin() ){
 					if( $deactivate ) {
